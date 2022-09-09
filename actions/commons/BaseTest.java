b17548681c06295b8b5a4293cfcaffcdbea64d8e
nguye <nguye@HuyenNTT>
2022-09-09 12:13:48 +0700

    Framework - 04 multiple browser

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -4,34 +4,71 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 
 	private WebDriver driverBaseTest;
 
-	private String projectPath = System.getProperty("user.dir");
-
 	protected WebDriver getBrowserDriver(String browserName) {
 
 		if (browserName.equals("firefox")) {
 
-			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-
+			WebDriverManager.firefoxdriver().setup();
+			
 			driverBaseTest = new FirefoxDriver();
+
+		} else if (browserName.equals("h_firefox")) {
+
+			WebDriverManager.firefoxdriver().setup();
+
+			FirefoxOptions options = new FirefoxOptions();
+
+			options.addArguments("-headless");
+
+			options.addArguments("windows-size=1920x1080");
+
+			driverBaseTest = new FirefoxDriver(options);
 
 		} else if (browserName.equals("chrome")) {
 
-			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
-
+			WebDriverManager.chromedriver().setup();
+			
 			driverBaseTest = new ChromeDriver();
 
-		} else if (browserName.equals("edge")) {
+		} else if (browserName.equals("h_chrome")) {
 
-			System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+			WebDriverManager.chromedriver().setup();
+
+			ChromeOptions options = new ChromeOptions();
+
+			options.addArguments("--headless");
+
+			options.addArguments("windows-size=1920x1080");
+
+			driverBaseTest = new ChromeDriver(options);
+
+		} else if (browserName.equals("edge")) {
+			
+			WebDriverManager.edgedriver().setup();
 
 			driverBaseTest = new EdgeDriver();
+			
+		} else if (browserName.equals("coccoc")) {
+			
+			WebDriverManager.chromedriver().driverVersion("104.0.5112.20").setup();
+			
+			ChromeOptions options = new ChromeOptions();
+			
+			options.setBinary("C:\\Program Files\\CocCoc\\Browser\\Application\\browser.exe");
+
+			driverBaseTest = new ChromeDriver(options);
+			
 		} else {
 
 			throw new RuntimeException("Browser name invalid");
